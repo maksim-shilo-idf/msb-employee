b86ba470818377636d6ee58170b582cc9fe7ae6e
Maksim Shilo <maksim.shilo@idftechnology.by>
2024-01-22 13:14:27 +0300

    initial commit

---
+++ src/main/kotlin/com/msb/employee/MsbEmployeeApplication.kt
@@ -0,0 +1,11 @@
+package com.msb.employee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsbEmployeeApplication
+
+fun main(args: Array<String>) {
+  runApplication<MsbEmployeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/msb/employee/MsbEmployeeApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.msb.employee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsbEmployeeApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+}
